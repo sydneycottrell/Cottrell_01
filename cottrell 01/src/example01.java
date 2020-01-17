ce7866764b49bd55b59a20bb9ee4c66bc0575fff
sydneycottrell <sydneycottrell@knights.ucf.edu>
2020-01-17 18:02:04 -0500

    my first commit

---
+++ cottrell 01/src/example01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ln = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = ln.next();
+		
+		System.out.println("Hello, " + name);
+	}
+
+}
